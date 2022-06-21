c8ea68044a49612cec289922b11a2b525fac403b
Agboniro Esther Jacinta <agbonirojacinta@gmail.com>
2022-06-21 16:42:27 +0100

    updated code

--- app/src/main/java/com/example/myjournal/home/ui/Home.kt
+++ app/src/main/java/com/example/myjournal/home/ui/Home.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myjournal.R
+import com.example.myjournal.databinding.FragmentHomeBinding
+import com.example.myjournal.home.adapters.JournalAdapter
 
 
 class Home : Fragment() {
+
+    private lateinit var homeBinding: FragmentHomeBinding
+    private lateinit var journalAdapter: JournalAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +25,24 @@ class Home : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        homeBinding = FragmentHomeBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return homeBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        homeBinding.journalRecyclerView.layoutManager =
+            LinearLayoutManager(requireActivity())
+
+        journalAdapter = JournalAdapter(this@Home)
+        homeBinding.journalRecyclerView.adapter = journalAdapter
+    }
 
 }
